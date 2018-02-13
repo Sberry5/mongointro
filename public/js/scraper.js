257785992ae0ac75680447e268d84d614fb81c84
Shannon Berry-Heller <shannonberry-heller@Shannons-MacBook.local>
2018-02-13 06:54:55 -0600

    ID issue

--- public/js/scraper.js
+++ public/js/scraper.js
@@ -4,19 +4,20 @@ $(document).ready(function () {
 $('body').on('click', '.saveButton', function(event) {
     console.log("Save button function triggered");
     var newNote = $(this).closest('article').find('.userNote').val().trim();
-    var articleID = $(this).attr('data-id');
+    var articleID = $(this).closest('article').find('._id.value').val();
+    console.log('articleID', articleID);
     console.log('new note', newNote);
-    // POST request to add a note
-    // $.ajax({
-    //   method: 'PUT',
-    //   url: '/savedArticles' + articleID,
-    //   data: {
-    // // Value from input
-    //     note: $('.userNote').val()
-    //   }
-    // }).done(function(data) {
-    // // Log the response
-    // console.log('data: ', data);
-    //   });
+    // PUT request to add a note
+    $.ajax({
+      method: 'PUT',
+      url: '/savedArticles' + articleID,
+      data: {
+    // Value from input
+        note: $('.userNote').val()
+      }
+    }).done(function(data) {
+    // Log the response
+    console.log('data: ', data);
+      });
     });
 });
